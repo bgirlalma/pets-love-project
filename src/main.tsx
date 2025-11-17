5a93843998bfb4588fd2e9b193e1aa8be6561248
Alina <akulina.alina1999gmail.com>
2025-11-17 14:31:58 +0200

    fixed path

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/pets/">
+    <BrowserRouter basename="/pets-love-project/">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <App />
